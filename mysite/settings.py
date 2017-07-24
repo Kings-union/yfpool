338da07b7bddb495b606af62f1cae1e17b3ca577
shermanchang <changwen11@163.com>
2017-07-24 16:20:45 +0800

    [DEV] add Gzip to compress content

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-
+# compress
 COMPRESS_ROOT = 'static/'
 STATIC_URL = '/static/'
 
